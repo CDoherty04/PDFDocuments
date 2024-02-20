f386bb43fed115d7279886a8bffbb740d30fbb92
Charlie Doherty <charlieedoherty@gmail.com>
2024-02-20 13:33:41 -0600

    Add topics.csv and use the data to create pages for each topic

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fpdf import FPDF
+import pandas as pd
 
 document = FPDF(orientation="P", unit="mm", format="A4")
 
-document.add_page()
-document.set_font(family="Times", size=15, style="B")
-document.cell(w=0, h=12, txt="TITLE", align="C", ln=1)
+df = pd.read_csv("topics.csv")
 
-document.set_font(family="Times", size=12)
-document.cell(w=0, h=12, txt="Chill", align="L", ln=1)
+for index, row in df.iterrows():
+    for i in range(int(row["Pages"])):
+        document.add_page()
+        document.set_font(family="Times", size=25, style="B")
+        document.set_text_color(10, 10, 50)
+        document.cell(w=0, h=12, txt=row["Topic"], align="C", ln=1)
+        document.line(10, 25, 200, 25)
 
 document.output("output.pdf")
