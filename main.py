ee760241c1e4280677ca852a9a1c373aff619729
Charlie Doherty <charlieedoherty@gmail.com>
2024-02-20 13:38:15 -0600

    Add documentation

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fpdf import FPDF
 import pandas as pd
 
+# Creates a pdf with portrait landscape
 document = FPDF(orientation="P", unit="mm", format="A4")
 
 df = pd.read_csv("topics.csv")
 
+# Read each row in topics.csv
 for index, row in df.iterrows():
+
+    # Create a certain number of new pages as specified by topics.csv
     for i in range(int(row["Pages"])):
+
+        # Adds formatting, text, and a break line to every page
         document.add_page()
         document.set_font(family="Times", size=25, style="B")
         document.set_text_color(10, 10, 50)
