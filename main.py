3c4ed3f60ed7fb40a060708b752e2faacf906172
Charlie Doherty <charlieedoherty@gmail.com>
2024-02-20 13:41:43 -0600

    Remove section title from every page except the first of its section

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ df = pd.read_csv("topics.csv")
 # Read each row in topics.csv
 for index, row in df.iterrows():
 
-    # Create a certain number of new pages as specified by topics.csv
-    for i in range(int(row["Pages"])):
+    # Adds the title page with formatting, text, and a break line
+    document.add_page()
+    document.set_font(family="Times", size=25, style="B")
+    document.set_text_color(10, 10, 50)
+    document.cell(w=0, h=12, txt=row["Topic"], align="C", ln=1)
+    document.line(10, 25, 200, 25)
 
-        # Adds formatting, text, and a break line to every page
+    # Create a certain number of empty pages as specified by topics.csv
+    for i in range(int(row["Pages"])-1):
         document.add_page()
-        document.set_font(family="Times", size=25, style="B")
-        document.set_text_color(10, 10, 50)
-        document.cell(w=0, h=12, txt=row["Topic"], align="C", ln=1)
-        document.line(10, 25, 200, 25)
 
 document.output("output.pdf")
