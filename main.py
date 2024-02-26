79eceaa8cd821282dd3952070cbb05506b567943
Charlie Doherty <charlieedoherty@gmail.com>
2024-02-25 21:25:21 -0600

    Add lines

--- main.py
+++ main.py
@@ -10,12 +10,11 @@ df = pd.read_csv("topics.csv")
 # Read each row in topics.csv
 for index, row in df.iterrows():
 
-    # Adds the title page with formatting, text, and a break line
+    # Adds the title page with formatting, text, and lines
     document.add_page()
     document.set_font(family="Times", size=25, style="B")
     document.set_text_color(10, 10, 50)
     document.cell(w=0, h=12, txt=row["Topic"], align="C", ln=1)
-    document.line(10, 25, 200, 25)
 
     # Create the footer
     document.ln(260)
@@ -23,7 +22,11 @@ for index, row in df.iterrows():
     document.set_text_color(180, 180, 180)
     document.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
 
-    # Create a certain number of empty pages as specified by topics.csv
+    # Add lines
+    for line in range(25):
+        document.line(10, (25 + 10 * line), 200, (25 + 10 * line))
+
+    # Create a certain number of pages without a header
     for i in range(int(row["Pages"])-1):
         document.add_page()
 
@@ -32,5 +35,9 @@ for index, row in df.iterrows():
         document.set_font(family="Times", style="I", size=10)
         document.set_text_color(180, 180, 180)
         document.cell(w=0, h=10, txt=row["Topic"], align="R", ln=1)
+
+        # Add lines
+        for line in range(25):
+            document.line(10, (25 + 10 * line), 200, (25 + 10 * line))
 
 document.output("output.pdf")
